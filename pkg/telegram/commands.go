dcd0e59d9f7da3f351f98f05b4f5849287aa7f53
Justin <justin@garofolo.net>
2022-08-18 05:05:49 -0400

    set/get commands

---
+++ pkg/telegram/commands.go
@@ -0,0 +1,101 @@
+package telegram
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+const (
+	DefaultLanguageCode = "en"
+)
+
+var (
+	BotCommandScopeAllPrivateChats IMap = IMap{"type": "all_private_chats"}
+)
+
+type BotCommand struct {
+	Command     string `json:"command"`     // Max 32 chars
+	Description string `json:"description"` // Max 256 chars
+}
+
+type SetMyCommandsResponse struct {
+	Ok          bool   `json:"ok"`
+	Result      bool   `json:"result"`
+	Description string `json:"description"`
+}
+
+type GetMyCommandsResponse struct {
+	Ok          bool         `json:"ok"`
+	Result      []BotCommand `mapstructure:"result"`
+	Description string       `json:"description"`
+}
+
+func (tc *TelegramClient) SetMyCommands(commands []BotCommand, scope *IMap, languageCode string) (bool, error) {
+	if scope == nil {
+		scope = &BotCommandScopeAllPrivateChats
+	}
+
+	if len(languageCode) == 0 {
+		languageCode = DefaultLanguageCode
+	}
+
+	request := IMap{
+		"commands":      commands,
+		"scope":         scope,
+		"language_code": languageCode,
+	}
+
+	fmt.Println(request)
+
+	resp, err := tc.SendRequest(POST, "setMyCommands", &request)
+	if err != nil {
+		return false, fmt.Errorf("SetMyCommands failed: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	var data SetMyCommandsResponse
+	err = json.NewDecoder(resp.Body).Decode(&data)
+
+	if data.Ok {
+		return data.Result, nil
+	} else if len(data.Description) > 0 {
+		return false, errors.New(data.Description)
+	}
+
+	return false, UnknownError
+}
+
+func (tc *TelegramClient) GetMyCommands(scope *IMap, languageCode string) (*[]BotCommand, error) {
+	if scope == nil {
+		scope = &BotCommandScopeAllPrivateChats
+	}
+
+	if len(languageCode) == 0 {
+		languageCode = DefaultLanguageCode
+	}
+
+	request := IMap{
+		"scope":         scope,
+		"language_code": languageCode,
+	}
+
+	resp, err := tc.SendRequest(POST, "getMyCommands", &request)
+	if err != nil {
+		return nil, fmt.Errorf("GetMyCommands failed: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	var data GetMyCommandsResponse
+	err = json.NewDecoder(resp.Body).Decode(&data)
+
+	if data.Ok {
+		return &data.Result, nil
+	} else if len(data.Description) > 0 {
+		return nil, errors.New(data.Description)
+	}
+
+	return nil, UnknownError
+}
