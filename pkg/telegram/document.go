f458b5a2631522089012c9857f5f1fe9c00417de
Justin <justin@garofolo.net>
2023-04-23 23:24:00 -0400

    add SendDocument func to TelegramClient

---
+++ pkg/telegram/document.go
@@ -0,0 +1,41 @@
+package telegram
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+// https://core.telegram.org/bots/api#senddocument
+func (tc *TelegramClient) SendDocument(chat int, document string, caption string, replyMarkup *IMap) (*Message, error) {
+	request := IMap{"chat_id": chat}
+
+	request["document"] = document // file url
+	request["caption"] = caption
+
+	if replyMarkup != nil {
+		request["reply_markup"] = *replyMarkup
+	}
+
+	resp, err := tc.SendRequest(POST, "sendDocument", &request)
+	if err != nil {
+		return nil, fmt.Errorf("SendDocument failed: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	var data SendMessageResponse
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return nil, fmt.Errorf("SendDocument failed: %w", err)
+	}
+
+	if data.Ok {
+		return &data.Result, nil
+	} else if len(data.Description) > 0 {
+		err = errors.New(data.Description)
+		return nil, fmt.Errorf("SendDocument failed: %w", err)
+	}
+
+	return nil, fmt.Errorf("SendDocument failed: %w", UnknownError)
+}
