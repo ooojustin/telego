fcb4f86cb4294afb4940575dfd8a5139689f9c74
Justin <justin@garofolo.net>
2023-05-07 21:53:03 -0400

    fix scope type in command funcs

--- pkg/telegram/commands.go
+++ pkg/telegram/commands.go
@@ -11,8 +11,12 @@ const (
 )
 
 var (
-	BotCommandScopeAllPrivateChats IMap = IMap{"type": "all_private_chats"}
+	BotCommandScopeAllPrivateChats Scope = Scope{Type: "all_private_chats"}
 )
+
+type Scope struct {
+	Type string `json:"type"`
+}
 
 type BotCommand struct {
 	Command     string `json:"command"`     // Max 32 chars
@@ -36,19 +40,18 @@ type DeleteMyCommandsResponse struct {
 }
 
 // https://core.telegram.org/bots/api#setmycommands
-func (tc *TelegramClient) SetMyCommands(commands []BotCommand, scope *IMap, languageCode string) (bool, error) {
-	if scope == nil {
-		scope = &BotCommandScopeAllPrivateChats
-	}
-
+func (tc *TelegramClient) SetMyCommands(commands []BotCommand, scope *Scope, languageCode string) (bool, error) {
 	if len(languageCode) == 0 {
 		languageCode = DefaultLanguageCode
 	}
 
 	request := IMap{
 		"commands":      commands,
-		"scope":         scope,
 		"language_code": languageCode,
+	}
+
+	if scope != nil {
+		request["scope"] = scope
 	}
 
 	resp, err := tc.SendRequest(POST, "setMyCommands", &request)
@@ -75,7 +78,7 @@ func (tc *TelegramClient) SetMyCommands(commands []BotCommand, scope *IMap, lang
 }
 
 // https://core.telegram.org/bots/api#getmycommands
-func (tc *TelegramClient) GetMyCommands(scope *IMap, languageCode string) (*[]BotCommand, error) {
+func (tc *TelegramClient) GetMyCommands(scope *Scope, languageCode string) (*[]BotCommand, error) {
 	if scope == nil {
 		scope = &BotCommandScopeAllPrivateChats
 	}
@@ -113,7 +116,7 @@ func (tc *TelegramClient) GetMyCommands(scope *IMap, languageCode string) (*[]Bo
 }
 
 // https://core.telegram.org/bots/api#deletemycommands
-func (tc *TelegramClient) DeleteMyCommands(scope *IMap, languageCode string) (bool, error) {
+func (tc *TelegramClient) DeleteMyCommands(scope *Scope, languageCode string) (bool, error) {
 	if scope == nil {
 		scope = &BotCommandScopeAllPrivateChats
 	}
