09cd7cce291ee4f7fa3e967afb396532702d6f95
Justin <justin@garofolo.net>
2023-05-19 08:06:16 -0400

    added mapstructure to user/bot user struct tags

--- pkg/telegram/user.go
+++ pkg/telegram/user.go
@@ -8,21 +8,21 @@ import (
 
 // https://core.telegram.org/bots/api#user
 type User struct {
-	ID                    int    `json:"id"`
-	IsBot                 bool   `json:"is_bot"`
-	FirstName             string `json:"first_name"`
-	LastName              string `json:"last_name"`
-	Username              string `json:"username"`
-	LanguageCode          string `json:"language_code"`
-	IsPremium             bool   `json:"is_premium"`
-	AddedToAttachmentMenu bool   `json:"added_to_attachment_menu"`
+	ID                    int    `json:"id" mapstructure:"id"`
+	IsBot                 bool   `json:"is_bot" mapstructure:"is_bot"`
+	FirstName             string `json:"first_name" mapstructure:"first_name"`
+	LastName              string `json:"last_name" mapstructure:"last_name"`
+	Username              string `json:"username" mapstructure:"username"`
+	LanguageCode          string `json:"language_code" mapstructure:"language_code"`
+	IsPremium             bool   `json:"is_premium" mapstructure:"is_premium"`
+	AddedToAttachmentMenu bool   `json:"added_to_attachment_men" mapstructure:"added_to_attachment_menu"`
 }
 
 type BotUser struct {
 	User
-	CanJoinGroups           bool `json:"can_join_groups"`
-	CanReadAllGroupMessages bool `json:"can_read_all_group_messages"`
-	SupportsInlineQueries   bool `json:"supports_inline_queries"`
+	CanJoinGroups           bool `json:"can_join_groups" mapstructure:"can_join_groups"`
+	CanReadAllGroupMessages bool `json:"can_read_all_group_messages" mapstructure:"can_read_all_group_messages"`
+	SupportsInlineQueries   bool `json:"supports_inline_queries" mapstructure:"supports_inline_queries"`
 }
 
 type GetMeResponse struct {
